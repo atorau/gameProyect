895b5aa2b0bdb12953079870c0e4136fb63c7527
Héctor Fernández Ardura <Hector@MacBook-de-Hector.local>
2017-04-17 16:00:57 +0200

    reconocimiento de notas funciona, problema con enlaces del navbar

--- logicalPart.js
+++ logicalPart.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-$('.uno').toggle();
+//$('.uno').toggle();
 $('.dos').toggle();
-// $('.tres').toggle();
+$('.tres').toggle();
+
 
 
   function PipeGame(){
@@ -20,47 +21,156 @@ $('.dos').toggle();
 
   }
 
-  var notes =[
-    {note:'do',sequence:[0,1,2,3,4,5,6,]},
-    {note:'re',sequence:[0,1,2,3,4,5]},
-    {note:'mi',sequence:[0,1,2,3,4,6]},
-    {note:'fa',sequence:[0,1,2,3,5,6]},
-    {note:'sol',sequence:[0,1,2,4,5,6]},
-    {note:'la',sequence:[0,1,4,5,6]},
-    {note:'si',sequence:[1,4,5,6]},
-    {note:'Do',sequence:[1,2,3,4,5,6]}
-  ];
+var notes =[
+  {note:'do',sequence:[0,1,2,3,4,5,6,]},
+  {note:'re',sequence:[0,1,2,3,4,5]},
+  {note:'mi',sequence:[0,1,2,3,4,6]},
+  {note:'fa',sequence:[0,1,2,3,5,6]},
+  {note:'sol',sequence:[0,1,2,4,5,6]},
+  {note:'la',sequence:[0,1,4,5,6]},
+  {note:'si',sequence:[1,4,5,6]},
+  {note:'Do',sequence:[1,2,3,4,5,6]},
+  {note:'WellDone',sequence:[1,2,3,4,5,6]}
+];
 
 
-this.actualNote=notes[5].sequence;
 
-this.sequence = [];
+
+
+var index = 0;
+var outputNote = notes[index].note;
+  $('.title3').html('<h2> Toca el "'+outputNote+'"</h2>');
+var actualNote = notes[index].sequence;
+
+//this.sequence = [];
 
 // if(this.sequence !== this.actualNote ? alert('lo haces mal') : alert('bien'));
 
 
 function changeframe(){
-    $(".Boton-Next").click(function(){
+    $(".Boton-Next1").click(function(){
       $(this).parent().parent().parent().toggle();
       $('.dos').toggle();
+    });
+
+    $(".Boton-Next2").click(function(){
+        $(this).parent().parent().toggle();
+        $('.tres').toggle();
+
+    $(".Inicio").click(function(){
+      $('.uno').show();
+    });
+    $(".LasNotas").click(function(){
+      $('.dos').show();
+    });
+    $(".Prueba").click(function(){
+      $('.tres').show();
+    });
+
 });
 }
 
 changeframe();
 
 
-//this.sequence.length===notes.secuence.length && this.sequence.every(function(v,i) { return v === notes.secuence[i]});
+var sequence=[];
 
 
 
+function chooseNote(){
+
+
+  $(".1T").click(function(){
+		$('.1T').css('z-index','-1');
+    sequence.push(0);
+	});
+
+  $(".1D").click(function(){
+		$('.1D').css('z-index','-1');
+    sequence.push(1);
+	});
+
+  $(".2D").click(function(){
+		$('.2D').css('z-index','-1');
+    sequence.push(2);
+	});
+
+  $(".3D").click(function(){
+		$('.3D').css('z-index','-1');
+    sequence.push(3);
+	});
+
+  $(".4D").click(function(){
+		$('.4D').css('z-index','-1');
+    sequence.push(4);
+	});
+
+  $(".5D").click(function(){
+		$('.5D').css('z-index','-1');
+    sequence.push(5);
+	});
+
+  $(".6D").click(function(){
+		$('.6D').css('z-index','-1');
+    sequence.push(6);
+	});
+
+  $(".7D").click(function(){
+		$('.7D').css('z-index','-1');
+    sequence.push(7);
+	});
+
+}
+
+chooseNote();
+
+function bien(index){
+  index ++;
+  outputNote = notes[index].note;
+  $('.title3').html('<h2> Toca el "'+outputNote+'"</h2>');
+  $('.1T').css('z-index','2');
+  $('.1D').css('z-index','3');
+  $('.2D').css('z-index','2');
+  $('.3D').css('z-index','2');
+  $('.4D').css('z-index','2');
+  $('.5D').css('z-index','2');
+  $('.6D').css('z-index','2');
+  $('.7D').css('z-index','2');
+  sequence=[];
+  actualNote = notes[index].sequence;
+  alert('Bien');
+  console.log(index);
+  $('.title3').html('<h2> Toca el "'+outputNote+'"</h2>');
+  if(index === 8){
+    $('.title3').html('<h2>"'+outputNote+'"</h2>');
+    alert('Bien hecho, ya sabes tocar una escala de "do" en Gaita');
+  }
+return index;
+}
 
 
 
+function yourChoose(){
+    $(".Boton-Next3").click(function(){
+      sequence = sequence.sort();
+      if(sequence.toString() === actualNote.toString()  ? index = bien(index) : alert('Vuelve a intentarlo'));
+
+    });
 
 
+    $(".Boton-Resset").click(function(){
+        $('.1T').css('z-index','2');
+        $('.1D').css('z-index','3');
+        $('.2D').css('z-index','2');
+        $('.3D').css('z-index','2');
+        $('.4D').css('z-index','2');
+        $('.5D').css('z-index','2');
+        $('.6D').css('z-index','2');
+        $('.7D').css('z-index','2');
+        sequence=[];
+});
+}
 
-
-
-
+yourChoose();
 
 });
