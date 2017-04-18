27755264922affb704911173c7b051e929b26f0b
Héctor Fernández Ardura <Hector@MacBook-de-Hector.local>
2017-04-18 12:40:58 +0200

    canción completa falta video

--- logicalPart.js
+++ logicalPart.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-$('.uno').toggle();
+//$('.uno').toggle();
 $('.dos').hide();
 $('.tres').hide();
-//$('.cuatro').hide();
-$('#notasH').hide();
+$('.row').hide();
+
+$('.cuatro').hide();
+
 
 
   function PipeGame(){
@@ -78,26 +80,39 @@ function changeframe(){
     $('.uno').hide();
     $('.dos').hide();
     $('.cuatro').hide();
+    $('.textoHimno').hide();
+    $('.row').show();
     $('.tres').show();
   });
   $(".Tema").click(function(){
     $('.uno').hide();
     $('.dos').hide();
     $('.tres').hide();
+    $('.row').hide();
     $('.cuatro').show();
+    $('.textoHimno').show();
   });
     $(".Boton-Next1").click(function(){
       $(this).parent().parent().parent().toggle();
+      $('.row').show();
       $('.dos').toggle();
     });
 
     $(".Boton-Next2").click(function(){
         $(this).parent().parent().toggle();
+        $('.row').show();
         $('.tres').toggle();
     });
     $(".Boton-Next4").click(function(){
+        $('.row').toggle();
+        $('.textoHimno').show();
         $(this).parent().parent().toggle();
+
         $('.cuatro').toggle();
+    });
+    $("#gpluslogin").click(function(){
+        $('.row').show();
+        $('.textoHimno').hide();
     });
 }
 
@@ -203,6 +218,114 @@ function yourChoose(){
 }
 
 yourChoose();
+
+
+
+var indexSong = 0;
+var outputNoteSong = song[indexSong].note;
+  $('.title4').html('<h2> Toca el "'+outputNote+'"</h2>');
+var actualNoteSong = song[indexSong].sequence;
+var sequenceSong=[];
+
+function chooseNoteSong(){
+
+
+  $(".1T").click(function(){
+		$('.1T').css('z-index','-1');
+    sequenceSong.push(0);
+	});
+
+  $(".1D").click(function(){
+		$('.1D').css('z-index','-1');
+    sequenceSong.push(1);
+	});
+
+  $(".2D").click(function(){
+		$('.2D').css('z-index','-1');
+    sequenceSong.push(2);
+	});
+
+  $(".3D").click(function(){
+		$('.3D').css('z-index','-1');
+    sequenceSong.push(3);
+	});
+
+  $(".4D").click(function(){
+		$('.4D').css('z-index','-1');
+    sequenceSong.push(4);
+	});
+
+  $(".5D").click(function(){
+		$('.5D').css('z-index','-1');
+    sequenceSong.push(5);
+	});
+
+  $(".6D").click(function(){
+		$('.6D').css('z-index','-1');
+    sequenceSong.push(6);
+	});
+
+  $(".7D").click(function(){
+		$('.7D').css('z-index','-1');
+    sequenceSong.push(7);
+	});
+
+}
+
+chooseNoteSong();
+
+
+
+function bienSong(indexSong){
+  index ++;
+  outputNote = song[index].note;
+  $('.title4').html('<h2> Toca el "'+outputNote+'"</h2>');
+  $('.1T').css('z-index','2');
+  $('.1D').css('z-index','3');
+  $('.2D').css('z-index','2');
+  $('.3D').css('z-index','2');
+  $('.4D').css('z-index','2');
+  $('.5D').css('z-index','2');
+  $('.6D').css('z-index','2');
+  $('.7D').css('z-index','2');
+  sequenceSong=[];
+  actualNoteSong = song[index].sequence;
+  alert('♪ ♪ ♪ Bien ♪ ♪ ♪');
+  console.log(indexSong);
+  $('.title4').html('<h2> Toca el "'+outputNote+'"</h2>');
+  if(index === 10){
+    $('.title4').html('<h2>"'+outputNote+'"</h2>');
+    alert('♩♪♫♬ Bien hecho, ya sabes tocar una escala de "do" en Gaita♬♫♪♩');
+  }
+return indexSong;
+}
+
+
+
+function yourChooseSong(){
+    $(".Boton-Next5").click(function(){
+      sequenceSong = sequenceSong.sort();
+      if(sequenceSong.toString() === actualNoteSong.toString()  ? indexSong = bienSong(indexSong) : alert('☠☠☠  Vuelve a intentarlo  ☠☠☠'));
+
+    });
+
+
+    $(".Boton-Resset").click(function(){
+        $('.1T').css('z-index','2');
+        $('.1D').css('z-index','3');
+        $('.2D').css('z-index','2');
+        $('.3D').css('z-index','2');
+        $('.4D').css('z-index','2');
+        $('.5D').css('z-index','2');
+        $('.6D').css('z-index','2');
+        $('.7D').css('z-index','2');
+        sequenceSong=[];
+});
+}
+
+yourChooseSong();
+
+
 
 
 
