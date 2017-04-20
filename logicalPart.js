5b0d5d982611c21e7ede0abc22a956f06d3c3945
Héctor Fernández Ardura <Hector@MacBook-de-Hector.local>
2017-04-20 13:01:13 +0200

    enlaces por voz

--- logicalPart.js
+++ logicalPart.js
@@ -383,8 +383,17 @@ yourChooseSong();
 if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
-    'Notas': function() {
-      $(".Boton-Next1").click();
+    'Ir a Inicio': function() {
+      $(".Inicio").click();
+    }
+    'Ir a Notas': function() {
+      $(".LasNotas").click();
+    }
+    'Ir a Prueba': function() {
+      $(".Prueba").click();
+    }
+    'Ir a Cancion': function() {
+      $(".Tema").click();
     }
   };
 
